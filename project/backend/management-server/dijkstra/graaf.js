b10afbdf304497f704ef5ee335ee62e22186ba32
unknown <Robbe.DeSutter@UGent.Be>
2019-03-05 09:40:43 +0100

    commit in correcte map

--- project/backend/management-server/dijkstra/graaf.js
+++ project/backend/management-server/dijkstra/graaf.js
@@ -10,21 +10,26 @@ class Graaf {
         console.log(this.knopen + 'zijn de knopen');
         console.log(this.verbindingen + 'zijn de verbindingen');
     }
-	
-	maakGrid(sizeX, sizeY){
-		for (let i = 0; i < sizeX; i++){
-			for (let j = 0; j < sizeY; j++){
-			this.voegKnopenToe([i+'X'+j+'Y']);
-			}
-		}
-		for (let i = 0; i < sizeX-1; i++) {
-			for (let j = 0; j < sizeY-1; j++) {
-				this.voegVerbindingenToe([[i+'X'+j+'Y',(i.valueOf()+1)+'X'+j+'Y',1]]);
-				this.voegVerbindingenToe([[i+'X'+j+'Y',i+'X'+(j.valueOf()+1)+'Y',1]]);
-				this.voegVerbindingenToe([[i+'X'+j+'Y',(i.valueOf()+1)+'X'+(j.valueOf()+1)+'Y',1]]);
-			}
-		}
-	}
+
+    maakGrid(sizeX, sizeY){
+        for (i = 0; i < sizeX; i++){
+            for (j = 0; j < sizeY; j++){
+                this.voegKnopenToe([i+'X'+j+'Y']);
+            }
+        }
+        for (i = 0; i < sizeX-1; i++) {
+            for (j = 0; j < sizeY-1; j++) {
+                this.voegVerbindingenToe([[i+'X'+j+'Y',(i.valueOf()+1)+'X'+j+'Y',1]]);
+                this.voegVerbindingenToe([[i+'X'+j+'Y',i+'X'+(j.valueOf()+1)+'Y',1]]);
+                this.voegVerbindingenToe([[i+'X'+j+'Y',(i.valueOf()+1)+'X'+(j.valueOf()+1)+'Y',1]]);
+            }
+        }
+        for (i = 0; i < sizeX-1; i++) {
+            for (j = 1; j < sizeY-1; j++) {
+                this.voegVerbindingenToe([[i+'X'+j+'Y',(i.valueOf()+1)+'X'+(j.valueOf()-1)+'Y',1]]);
+            }
+        }
+    }
 
     //toevoegen van knopen op de graaf adhv een array.
     voegKnopenToe(knopen) {
