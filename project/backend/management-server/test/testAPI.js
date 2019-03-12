789d4c7eeb0a9816145cda9cb0cc356dfb797668
Jochen Laroy <jochen.laroy@ugent.be>
2019-03-12 08:58:45 +0100

    Update testAPI.js

--- project/backend/management-server/test/testAPI.js
+++ project/backend/management-server/test/testAPI.js
@@ -9,7 +9,7 @@ chai.should();
 describe("Map", function() {
     describe("GET /", function() {
         //test om de map op te halen
-        it("Zou de map moeten opgehaald hebben", function(done) {
+        it("Zou alle mappen moeten opgehaald hebben", function(done) {
             chai.request(app)
                 .get('/api/maps')
                 .end((err, res) => {
@@ -18,5 +18,16 @@ describe("Map", function() {
                     done();
                 });
         }).timeout(5000);
+
+        it("Zou 1 enkele map moeten ophalen met een specifiek ID", function(done) {
+            //zelfde id als de map waarop getest wordt in dijkstra
+            const id = "5c851e1c392e923b60fff836";
+            chai.request(app)
+                .get(`api/maps/${id}`)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.an('array');
+                })
+        })
     });
 });
