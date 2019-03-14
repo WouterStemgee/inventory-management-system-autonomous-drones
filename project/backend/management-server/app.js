e2be49859bbe00ce7e2dd3946acda95602955226
Jochen Laroy <jochen.laroy@ugent.be>
2019-03-14 16:27:46 +0100

    nodered module toegevoegd + runt maar editor ngo fixen

--- project/backend/management-server/app.js
+++ project/backend/management-server/app.js
@@ -26,10 +26,6 @@ app.use('/api/maps', mapRouter);
 app.use('/api/drone', droneRouter);
 app.use('/api/flightpath', waypointRouter);
 
-//route voor statische context -> node-red, mss niet meer nodig door de vorige call met directory name
-//app.use("/node-red",express.static("public"));
-
-
 // je hebt MongoDB lokaal geinstalleerd, indien deze nog niet bestaat wordt dit automatisch aangemaakt
 mongoose.connect('mongodb://localhost/drone1', {useNewUrlParser: true});
 
