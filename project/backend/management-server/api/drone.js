b192abffd6d386fd2f3e78722e079a53c5313b81
Jochen Laroy <jochen.laroy@ugent.be>
2019-03-19 10:48:30 +0100

    mqtt module weggedaan en module toegevoegd

--- project/backend/management-server/api/drone.js
+++ project/backend/management-server/api/drone.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const client = require('../mqtt/client');
 
 const router = express.Router();
 
--- project/backend/management-server/api/flightpath.js
+++ project/backend/management-server/api/flightpath.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const client = require('../mqtt/client');
 const dijkstra = require('../app');
 
 const router = express.Router();
