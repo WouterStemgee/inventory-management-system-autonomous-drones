8c980a5d55502fb0ae1c2ac099041fd6564f715f
Karel Everaert <karevera.everaert@ugent.be>
2019-03-21 16:13:10 +0100

    aangepast zodat het meer modulair is

--- project/pythonMockup/client.py
+++ project/pythonMockup/client.py
@@ -6,40 +6,52 @@ from Drone import Drone
 
 drone = Drone(1,5,6)# hier nog een betere manier voor zoeken
 
-def on_log(client,userdata,level,buf):
-    print("log: "+buf)
+class Client:
+    def __init__(self):
+        def on_log(client, userdata, level, buf):
+            print("log: " + buf)
 
-def on_connect(client,userdata,flags,rc):
-    if rc == 0:
-        print("connected oke")
-    else:
-        print("Bad connection, returned code=",rc)
+        def on_connect(client, userdata, flags, rc):
+            if rc == 0:
+                print("connected oke")
+            else:
+                print("Bad connection, returned code=", rc)
 
-def on_disconnect(client,userdata,flags,rc=0):
-    print("Disconnected result code "+str(rc))
+        def on_disconnect(client, userdata, flags, rc=0):
+            print("Disconnected result code " + str(rc))
 
-def on_message(client,userdata,msg):
-    topic = msg.topic
-    m_decode = str(msg.payload.decode("utf-8","ignore"))
-    print("message received",m_decode)
+        def on_message(client, userdata, msg):
+            topic = msg.topic
+            m_decode = str(msg.payload.decode("utf-8", "ignore"))
+            print("message received", m_decode)
+        self.client = mqtt.Client("python1")
+        self.broker = "test.mosquitto.org"
+        self.client.on_connect = on_connect
+        self.client.on_disconnect = on_disconnect
+        self.client.on_message = on_message
 
 
-broker="test.mosquitto.org" # volgens mij zit hier nog een fout, waardoor al de rest faalt
-client = mqtt.Client("python1")
+
+    #broker = "test.mosquitto.org"  # volgens mij zit hier nog een fout, waardoor al de rest faalt
+    #client = mqtt.Client("python1")
+
+    # client.on_log = on_log
+    #client.on_connect = on_connect
+    #client.on_disconnect = on_disconnect
+    #client.on_message = on_message
+
+    def stuurBattery(self):
+        print("connecting to broker ", self.broker)
+
+        self.client.connect(self.broker)
+        self.client.loop_start()  # moet lopen om de callback functies te kunnen verbinden
+        self.client.subscribe("drone/battery")
+        self.client.publish("drone/battery", drone.get_battery())  # eerste argument is het topic, 2de is de message
+        time.sleep(4)
+        self.client.loop_stop()
+        self.client.disconnect()
 
 
-#client.on_log = on_log
-client.on_connect = on_connect
-client.on_disconnect = on_disconnect
-client.on_message = on_message
 
-print("connecting to broker ",broker)
-
-client.connect(broker)
-client.loop_start() # moet lopen om de callback functies te kunnen verbinden
-client.subscribe("drone/battery")
-client.publish("drone/battery",drone.get_battery()) # eerste argument is het topic, 2de is de message
-time.sleep(4)
-client.loop_stop()
-client.disconnect()
-
+client = Client()
+client.stuurBattery()
