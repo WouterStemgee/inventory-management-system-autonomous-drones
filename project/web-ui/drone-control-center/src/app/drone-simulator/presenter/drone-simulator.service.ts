37c8201ea5ac84fcd8fcc318b782a3570ea15163
wstemge <Wouter.Stemgee@UGent.be>
2019-04-16 12:41:38 +0200

    small fix

--- project/web-ui/drone-control-center/src/app/drone-simulator/presenter/drone-simulator.service.ts
+++ project/web-ui/drone-control-center/src/app/drone-simulator/presenter/drone-simulator.service.ts
@@ -127,6 +127,7 @@ export class DroneSimulatorService {
                           this.http.getAllDrones()
                             .then(newDrones => {
                               this.drones = newDrones;
+                              resolve();
                             })
                             .catch(error => {
                               this.onAlertEvent.emit({
