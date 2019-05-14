98999740f6272f8e1a5d1114945af1a1557619dd
rmoeyers <Roel.Moeyersoons@ugent.be>
2019-05-14 09:01:26 +0200

    drone db aanpassing (backend enkel)

--- project/backend/management-server/database/models/drone.js
+++ project/backend/management-server/database/models/drone.js
@@ -1,9 +1,20 @@
 const mongoose = require('mongoose');
 
+const coordSchema = mongoose.Schema({
+    _id: false,
+    x: {type: Number, required: true},
+    y: {type: Number, required: true}
+});
+
 const droneSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     name: {type: String, required: true},
-    properties: {radius: {type: Number, required: true}}
+    properties: {
+        radius: {type: Number, required: true},
+        homebase: coordSchema,
+        maxbattery: Number,
+        batteryusage: Number
+    }
 
 });
 
